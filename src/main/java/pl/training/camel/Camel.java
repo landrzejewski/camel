1cc36c1f5f7025cac3014274ce2bd7a9f1944371
lukas <landrzejewski.poczta@gmail.com>
2023-09-18 15:13:06 +0200

    first commit

--- src/main/java/pl/training/camel/Camel.java
+++ src/main/java/pl/training/camel/Camel.java
@@ -1,17 +1,22 @@
 package pl.training.camel;
 
 import org.apache.camel.impl.DefaultCamelContext;
-import org.apache.camel.main.Main;
+import org.apache.camel.component.activemq.ActiveMQComponent;
 
 // https://github.com/landrzejewski/camel
 
 public class Camel {
 
     public static void main(String[] args) {
+
+        var jmsComponent = new ActiveMQComponent();
+        jmsComponent.setBrokerURL("tcp://localhost:61616");
+
         try (var context = new DefaultCamelContext()) {
             var propertiesComponent = context.getPropertiesComponent();
             propertiesComponent.setLocation("classpath:training.properties");
             context.getRegistry().bind("ymlFilter", new YmlFileFilter<>());
+            context.addComponent("customJms", jmsComponent);
             context.addRoutes(new Routes());
             context.start();
             Thread.sleep(10_000);
@@ -31,33 +36,6 @@ public class Camel {
         } catch (IOException e) {
             System.out.println("Exception: " + e.getMessage());
         }*/
-
-        /*
-        // System.out.println("Hello World");
-
-        User user = new User("Marcin");
-        // user.setFirstName("Jan");
-        user.sayHello();
-        */
-    }
-
-}
-
-class User {
-
-    private String firstName;
-    private int age;
-
-    public User(String name) {
-        firstName = name;
-    }
-
-    public void sayHello() {
-        System.out.println("Hi, I am " + firstName);
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
     }
 
 }
--- src/main/java/pl/training/camel/Routes.java
+++ src/main/java/pl/training/camel/Routes.java
@@ -6,14 +6,21 @@ public class Routes extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
-
         // env:VARIABLE_NAME // using ENV
-        // RAW(value) // escaping invalid uri value
+        // RAW(value) // escaped uri value
         // #name // accessing registered element from context
         from("file:data/input?noop=true&delay={{file.interval}}&filter=#ymlFilter")
                 //.filter(new FileTypePredicate())
                 .process(new LoggingProcessor())
                 .to("file:data/output");
+
+        from("ftp://localhost?delay=5000&delete=true&passiveMode=true&username=user&password=123")
+                .to("activemq:orders");
+                //.toD("jms:queue:${header.finalDestination}"); // dynamic destination
+
+        from("customJms:orders")
+                .log("Message received: ${header.CamelFileName}")
+                .to("log:pl.training.camel");
     }
 
 }
